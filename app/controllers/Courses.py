2e13b7209618b4ad1df4cc429cd2e416536c71dc
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-19 16:27:57 -0400

    modified methods for functionality. extra methods are listed for future projects

--- app/controllers/Courses.py
+++ app/controllers/Courses.py
@@ -5,18 +5,15 @@ class Courses(Controller):
         super(Courses, self).__init__(action)
         # Note that we have to load the model before using it
         self.load_model('Course')
-
     def index(self):
         course = self.models['Course'].get_all_courses()
         return self.load_view('index.html', course=course)
-
     # This is how a method with a route parameter that provides the id would work
     # We would set up a GET route for this method
     def show(self, id):
         # Note how we access the model using self.models
         course = self.models['Course'].get_course_by_id(id)
         return self.load_view('show.html', course=course)
-
     def add(self):
         if len(request.form['name']) > 15:
             course_details = {
@@ -28,7 +25,6 @@ class Courses(Controller):
         else:
             flash('longer than 15')
             return redirect('/')
-
     # This is how a method used to update a course would look
     # We would set up a POST route for this method
     def update(self, course_id):
@@ -41,14 +37,12 @@ class Courses(Controller):
         }
         self.models['Course'].update_course(course_details)
         return redirect('/')
-
      # This is how a method used to delete a course would look
      # We would set up a POST route for this method
     def delete_check(self,course_id):
         course = self.models['Course'].get_course_by_id(course_id)
         print course
         return self.load_view('delete_me', course=course)
-
     def delete(self, course_id):
         print "Oh yeah..."
         self.models['Course'].delete_course(course_id)
