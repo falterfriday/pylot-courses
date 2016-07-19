8bd0545b112a8544f70352aeb2fdfbf04bb42c4b
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-19 16:29:32 -0400

    successfully hooked into mysql, allowing db calls from models to pass querys through to the controllers

--- app/models/Course.py
+++ app/models/Course.py
@@ -15,19 +15,16 @@ class Course(Model):
         super(Course, self).__init__()
     def get_all_courses(self):
         return self.db.query_db("SELECT * FROM courses ORDER BY created_at DESC")
-
     def get_course_by_id(self, course_id):
         # pass data to the query like so
         query = "SELECT * FROM courses WHERE id = :course_id"
         data = { 'course_id': course_id}
         return self.db.query_db(query, data)
-
     def add_course(self, course):
       # Build the query first and then the data that goes in the query
       query = "INSERT INTO courses (title, description, created_at) VALUES (:title, :description, NOW())"
       data = { 'title': course['title'], 'description': course['description'] }
       return self.db.query_db(query, data)
-
     def update_course(self, course):
       # Building the query for the update
       query = "UPDATE courses SET title=:title, description=:description WHERE id = :course_id"
@@ -35,7 +32,6 @@ class Course(Model):
       data = { 'title': course['title'], 'description': course['description'], 'course_id': course['id']}
       # run the update
       return self.db.query_db(query, data)
-
     def delete_course(self, course_id):
       query = "DELETE FROM courses WHERE id = :course_id"
       data = { "course_id": course_id }
